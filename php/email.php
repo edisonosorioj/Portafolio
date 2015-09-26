5440cc7e9d5eb804c29d2f524c533f864223622a
Edison Osorio <edisonosorio@nusoft.com.co>
2015-09-26 09:57:20 -0500

    Mejoras en Email
    Para que funcione hay que configurar el EC2 para que sea SHTP

--- php/email.php
+++ php/email.php
@@ -1,21 +1,27 @@
 <?php
 
-$nombre = $_POST["nombre"];
-$de = $_POST["email"];
-$mensaje = $_POST["motivo"];
+$nombre = addslashes($_POST["nombre"]);
+$email = addslashes($_POST["email"]);
+$mensaje = addslashes($_POST["motivo"]);
 
-if (!empty($nombre) AND !empty($mensaje) AND !empty($de)) {
-// $headers = "MIME-Version:1.0;\r\n";
-$para = "edisonosorioj@gmail.com";
-$headers = "Content-Type: text/html; charset=UTF-8 \r\n";
-$headers .= "From: $de;". "\r\n";
-$asunto = "Contacto desde el sitio Website de: $nombre";
-// $headers .= "To: $para;. \r\n .Subject:$asunto;. \r\n";
+$cabeceras = "From: $email\n"
+ .  "Reply-To: $email\n";
+$asunto = "Mensaje desde la pagina Web";
+$email_to = "edisonosorioj@gmail.com";
+$contenido = "$nombre a enviado un mensaje desde el sitio Website\n"
+. "\n"
+. "Nombre: $nombre\n"
+. "Email: $email\n"
+. "Mensaje: $mensaje\n"
+. "\n";
 
-@mail($para,$asunto,$mensaje,$headers);
-	echo "Mensaje enviado correctamente<br /><a href='../index.html'>Volver</a>";
-} else {
-	echo "El mensaje no fue enviado. Intentalo de nuevo";
+if (mail($email_to, $asunto ,$contenido ,$cabeceras )) {
+ 
+die("Gracias, su mensaje se envio correctamente.<br /><a href='index.html'>Volver</a>");
+}else{
+ 
+die("Error: Su informaci&oacute;n no pudo ser enviada, 
+	intente m&aacute;s tarde");
 }
 
 ?>
